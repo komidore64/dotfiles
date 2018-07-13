6ccb0a04f5998cdd029d71845180159c99f4c20f
Adam Price <komidore64@gmail.com>
2018-07-13 14:13:47 -0400

    add hyper.js terminal

--- dotfiles_installer.rb
+++ dotfiles_installer.rb
@@ -66,7 +66,8 @@ module Dotfiles
         "fonts" => ".fonts",
         "config/sublime-text-2" => ".config/sublime-text-2",
         "Xdefaults" => ".Xdefaults",
-        "Xmodmap" => ".Xmodmap"
+        "Xmodmap" => ".Xmodmap",
+        "hyper.js" => ".hyper.js"
       },
       :osx => { # saving osx for later. just want to get this finished for now
         # "config/sublime-text-2" => "Library/Application\ Support/Sublime\ Text\ 2",
