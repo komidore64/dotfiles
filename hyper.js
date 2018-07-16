ec30e1df14b062f54733de2b77a0be23cf98585d
Adam Price <komidore64@gmail.com>
2018-07-16 09:05:43 -0400

    hyper.js configs
    - blinking cursor
    - set some explicit defaults

--- hyper.js
+++ hyper.js
@@ -24,13 +24,13 @@ module.exports = {
     cursorColor: 'rgba(248,28,229,0.8)',
 
     // terminal text color under BLOCK cursor
-    cursorAccentColor: '#000',
+    cursorAccentColor: '#2E3436',
 
     // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
     cursorShape: 'BLOCK',
 
     // set to `true` (without backticks and without quotes) for blinking cursor
-    cursorBlink: false,
+    cursorBlink: true,
 
     // color of the text
     foregroundColor: '#ededed',
@@ -43,7 +43,7 @@ module.exports = {
     selectionColor: 'rgba(248,28,229,0.3)',
 
     // border color (window, tabs)
-    borderColor: '#333',
+    borderColor: '#4174FB',
 
     // custom CSS to embed in the main window
     css: '',
@@ -53,12 +53,12 @@ module.exports = {
 
     // if you're using a Linux setup which show native menus, set to false
     // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: '',
+    showHamburgerMenu: true,
 
     // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
     // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: '',
+    showWindowControls: true,
 
     // custom padding (CSS format, i.e.: `top right bottom left`)
     padding: '12px 14px',
